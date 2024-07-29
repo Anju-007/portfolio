21c98140295eb4a7bf18945dc20f8e3eb3dfd8e8
Anju-007 <anjuashokabhavan2000@gmail.com>
2024-07-29 17:47:07 +0530

    first commit to the clone

---
+++ Index/main.js
@@ -0,0 +1,9 @@
+var typed= new typed(".text", {
+    Strings:["Software Developer" , "Java Developer" , "Frontend Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+    
+
